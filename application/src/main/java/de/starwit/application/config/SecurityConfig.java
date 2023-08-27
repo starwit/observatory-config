86df7ba85dfdf6b72a8852083f8e47a88656adfe
witchpou <anett_huebner@gmx.de>
2023-08-27 15:20:15 +0200

    refactor: remve vscode warnings

--- application/src/main/java/de/starwit/application/config/SecurityConfig.java
+++ application/src/main/java/de/starwit/application/config/SecurityConfig.java
@@ -35,7 +35,6 @@ import java.io.IOException;
 import java.util.*;
 import java.util.function.Supplier;
 
-
 @Profile("auth")
 @Configuration
 @EnableWebSecurity
@@ -47,8 +46,8 @@ public class SecurityConfig {
     private ClientRegistrationRepository clientRegistrationRepository;
 
     LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
-                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(
+                this.clientRegistrationRepository);
 
         // Sets the location that the End-User's User Agent will be redirected to
         // after the logout has been performed at the Provider
@@ -69,30 +68,30 @@ public class SecurityConfig {
                 }))
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
-                )
+                        .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/**").hasAnyRole("admin", "user", "reader")
-                        .anyRequest().authenticated()
-                )
-                .logout((logout) -> logout
+                        .anyRequest().authenticated())
+                .logout(logout -> logout
                         .logoutSuccessHandler(oidcLogoutSuccessHandler())
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                )
-                // Maybe https://stackoverflow.com/questions/74939220/classnotfoundexception-org-springframework-security-oauth2-server-resource-web
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")))
+                // Maybe
+                // https://stackoverflow.com/questions/74939220/classnotfoundexception-org-springframework-security-oauth2-server-resource-web
                 .oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 
-
-    // Taken from https://stackoverflow.com/questions/74939220/classnotfoundexception-org-springframework-security-oauth2-server-resource-web
+    // Taken from
+    // https://stackoverflow.com/questions/74939220/classnotfoundexception-org-springframework-security-oauth2-server-resource-web
     @Component
     @RequiredArgsConstructor
     static class GrantedAuthoritiesMapperImpl implements GrantedAuthoritiesMapper {
 
+        @SuppressWarnings("unchecked")
         @Override
-        public Collection<? extends GrantedAuthority> mapAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        public Collection<? extends GrantedAuthority> mapAuthorities(
+                Collection<? extends GrantedAuthority> authorities) {
             Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
             authorities.forEach(authority -> {
@@ -106,11 +105,13 @@ public class SecurityConfig {
 
                         mappedAuthorities.addAll(roles.stream().map(SimpleGrantedAuthority::new).toList());
                     } catch (NullPointerException e) {
-                        LOG.error("Could not read the roles from claims.realm_access.roles -- Is the Mapper set up correctly?");
+                        LOG.error(
+                                "Could not read the roles from claims.realm_access.roles -- Is the Mapper set up correctly?");
                     }
                 } else if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
                     final var userAttributes = oauth2UserAuthority.getAttributes();
-                    final Map<String, List<String>> realmAccess = (Map<String, List<String>>) userAttributes.get("realm_access");
+                    final Map<String, List<String>> realmAccess = (Map<String, List<String>>) userAttributes
+                            .get("realm_access");
                     final List<String> roles = realmAccess.get("roles");
 
                     mappedAuthorities.addAll(roles.stream().map(SimpleGrantedAuthority::new).toList());
@@ -122,10 +123,10 @@ public class SecurityConfig {
 
     }
 
-
 }
 
-// Taken from https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-integration-javascript-spa
+// Taken from
+// https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#csrf-integration-javascript-spa
 final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
 
     private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
@@ -133,7 +134,8 @@ final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
         /*
-         * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
+         * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection
+         * of
          * the CsrfToken when it is rendered in the response body.
          */
         this.delegate.handle(request, response, csrfToken);
@@ -142,9 +144,12 @@ final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler 
     @Override
     public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
         /*
-         * If the request contains a request header, use CsrfTokenRequestAttributeHandler
-         * to resolve the CsrfToken. This applies when a single-page application includes
-         * the header value automatically, which was obtained via a cookie containing the
+         * If the request contains a request header, use
+         * CsrfTokenRequestAttributeHandler
+         * to resolve the CsrfToken. This applies when a single-page application
+         * includes
+         * the header value automatically, which was obtained via a cookie containing
+         * the
          * raw CsrfToken.
          */
         if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
