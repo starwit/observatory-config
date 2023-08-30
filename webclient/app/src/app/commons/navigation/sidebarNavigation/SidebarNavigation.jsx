58778c6b2a672dc54a6be53e2273cc7b14bf81b8
Florian Duderstadt <flori.duderstadt@gmail.com>
2023-08-30 11:18:51 +0200

    add animation to show/hide navigationbar

--- webclient/app/src/app/commons/navigation/sidebarNavigation/SidebarNavigation.jsx
+++ webclient/app/src/app/commons/navigation/sidebarNavigation/SidebarNavigation.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-    AppBar,
     Box,
     CssBaseline,
     Drawer,
@@ -9,84 +8,145 @@ import {
     ListItem,
     ListItemButton,
     ListItemText,
+    Stack,
     Toolbar,
     Typography
 } from "@mui/material";
-import {Logout} from "@mui/icons-material";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { styled, useTheme } from '@mui/material/styles';
+import { Logout } from "@mui/icons-material";
 import HeaderStyles from "../../../assets/styles/HeaderStyles";
-import {useTranslation} from "react-i18next";
-import {useHistory} from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import Divider from '@mui/material/Divider';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    }), 
+  );
+  
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+  
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
 
 function SidebarNavigation(props) {
 
     const headerStyles = HeaderStyles();
     const drawerWidth = 240;
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const history = useHistory();
 
-    function generateBoxStyles() {
-        let styles = {flexGrow: 1}
-        if (!props.focusMode) {
-            styles.p = 3
-        }
-        return styles
-    }
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
 
-    function renderAppBar() {
-        if (props.focusMode) {
-            return;
-        }
-        return (
-            <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar className={headerStyles.toolbar}>
-                    <img className={headerStyles.menuLogoImg} src={props.logo} alt="Logo of lirejarp"/>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <img className={headerStyles.menuLogoImg} src={props.logo} alt="Logo of lirejarp" />
                     <Typography variant="h6" noWrap>
                         {props.title}
                     </Typography>
-                    <div className={headerStyles.spacer}/>
+                    <div className={headerStyles.spacer} />
                     <IconButton color="secondary" disableRipple className={headerStyles.linkButton}
-                                onClick={() => history.push("/logout")}><Logout/></IconButton>
+                        onClick={() => history.push("/logout")}><Logout /></IconButton>
                 </Toolbar>
             </AppBar>
-        )
-    }
-
-    function renderAppBarSpacer() {
-        if (props.focusMode) {
-            return;
-        }
-        return (
-            <Toolbar className={headerStyles.toolbar}/>
-        )
-    }
-
-    return (
-        <Box sx={{display: 'flex'}}>
-            <CssBaseline/>
-            {renderAppBar()}
             <Drawer
-                variant="permanent"
                 sx={{
                     width: drawerWidth,
                     flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: {width: drawerWidth, boxSizing: 'border-box'},
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
                 }}
+                variant="persistent"
+                anchor="left"
+                open={open}
             >
-                {renderAppBarSpacer()}
-                <Box sx={{overflow: 'auto'}}>
+                <Toolbar />
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <Box sx={{ overflow: 'auto' }}>
                     <List>
                         {props.menuItems.map((menuItem, index) => (
                             <ListItem key={menuItem.title} disablePadding>
                                 <ListItemButton onClick={() => history.push(menuItem.link)}>
-                                    <ListItemText primary={t(menuItem.title)}/>
+                                    <ListItemText primary={t(menuItem.title)} />
                                 </ListItemButton>
                             </ListItem>
                         ))}
                     </List>
                 </Box>
             </Drawer>
-            <Box component="main" sx={generateBoxStyles()}>
-                {renderAppBarSpacer()}
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Toolbar className={headerStyles.toolbar} />
                 {props.children}
             </Box>
         </Box>
