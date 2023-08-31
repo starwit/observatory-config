8be0a5b5ea7f901835dc40eb53fcde143789a340
Cedric Rische <cedric.rische@cedsoft.de>
2023-08-31 22:03:05 +0200

    Removed console logs

--- webclient/app/src/app/features/imageAnnotate/ImageAnnotate.jsx
+++ webclient/app/src/app/features/imageAnnotate/ImageAnnotate.jsx
@@ -4,7 +4,7 @@ import ClassificationRest from "../../services/ClassificationRest";
 import {Typography} from "@mui/material";
 import ImageRest from "../../services/ImageRest";
 
-function ImageAnnotate(props){
+function ImageAnnotate(props) {
 
     const [classifications, setClassifications] = useState(null);
     const [images, setImages] = useState(null);
@@ -14,117 +14,85 @@ function ImageAnnotate(props){
 
     useEffect(() => {
         classificationRest.findAll().then(response => {
-            setClassifications(response.data)
-        })
-    }, [])
+            setClassifications(response.data);
+        });
+    }, []);
 
     useEffect(() => {
         imageRest.findAll().then(response => {
-            setImages(response.data)
-        })
-    }, [])
+            setImages(response.data);
+        });
+    }, []);
 
     useEffect(() => {
-        if (!images){
-            return
+        if (!images) {
+            return;
         }
         const newImages = [...images];
         const createdParsedImages = newImages.map(image => {
-            console.log("parsing", image)
             return {
                 id: image.id,
-                src: window.location.pathname + "api/imageFile/name/"+image.src,
+                src: window.location.pathname + "api/imageFile/name/" + image.src,
                 name: image.name,
                 regions: image.polygon?.map(poly => {
                     return {
                         type: "polygon",
                         id: poly.id,
-                        color:poly.classifications[0]?.color,
+                        color: poly.classifications[0]?.color,
                         cls: poly.classifications[0]?.name,
                         open: poly.open,
                         points: poly.points?.map(point => [point.xvalue, point.yvalue])
-                    }
+                    };
                 })
-            }
-        })
-        console.log("CreatedParsedImages",createdParsedImages);
-        setParsedImages(createdParsedImages)
-    }, [images])
+            };
+        });
+        setParsedImages(createdParsedImages);
+    }, [images]);
 
-
-    /*
-    {
-        src:
-        namne:
-        polygon: [
-            {
-                open: bool
-                classification: [
-                    {
-                        name:
-                    }
-                ]
-                point: [
-                    {
-                        xvalue:
-                        yvalue
-                    }
-                ]
-            }
-        ]
-        parkingConfig:{}
-    }
-     */
-    function saveImageAnnotations(evnt){
+    function saveImageAnnotations(evnt) {
         const localImages = evnt.images;
         const savePreparedImages = images.map(image => {
             return {
                 ...image,
                 polygon: localImages.find(imageFind => imageFind.id === image.id).regions.map(region => {
-                    return {
-                        classification:  (typeof region.cls == "object" && region.cls.isArray) ? region.cls.map(cl => classifications.find(classification => classification.name === cl)) : [classifications.find(classification => classification.name === region.cls)] ,
-                        points: region.points.map(point => {
-                            return {
-                                xvalue: point[0],
-                                yvalue: point[1]
-                            }
-                        })
-                    }
+                        return {
+                            classification: (typeof region.cls == "object" && region.cls.isArray) ? region.cls.map(cl => classifications.find(classification => classification.name === cl)) : [classifications.find(classification => classification.name === region.cls)],
+                            points: region.points.map(point => {
+                                return {
+                                    xvalue: point[0],
+                                    yvalue: point[1]
+                                };
+                            })
+                        };
                     }
                 )
-            }
-        })
-        console.log("beforePrepImages",evnt.images)
-        console.log("beforePrepEvent",evnt.images)
-        console.log("SavedPrepped images", savePreparedImages)
-        savePreparedImages.map(image => imageRest.create(image))
-        console.log(evnt.images.map(image => image.regions))
-        console.log(evnt)
+            };
+        });
+        savePreparedImages.map(image => imageRest.create(image));
     }
 
-    if (!classifications || !parsedImages){
-        return <Typography>Loading</Typography>
+    if (!classifications || !parsedImages) {
+        return <Typography>Loading</Typography>;
     }
 
-    if (parsedImages.length === 0){
-        return <Typography>No images created. Please add some first</Typography>
+    if (parsedImages.length === 0) {
+        return <Typography>No images created. Please add some first</Typography>;
     }
 
-    return(
+    return (
         <>
-            {console.log("IMAGES", parsedImages)}
-
-        <ReactImageAnnotate
-            labelImages
-            regionClsList={classifications.map(classification => classification.name)}
-            onExit={saveImageAnnotations}
-            hideNext
-            hidePrev
-            enabledTools={["select", "create-point", "create-polygon", "create-box", "create-line"]}
-            images={parsedImages}
-            hideHeaderText
-        />
+            <ReactImageAnnotate
+                labelImages
+                regionClsList={classifications.map(classification => classification.name)}
+                onExit={saveImageAnnotations}
+                hideNext
+                hidePrev
+                enabledTools={["select", "create-point", "create-polygon", "create-box", "create-line"]}
+                images={parsedImages}
+                hideHeaderText
+            />
         </>
-    )
+    );
 }
-export default ImageAnnotate
+
+export default ImageAnnotate;
