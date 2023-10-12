8c3d0d857103a49cb62d1b51095663a41ac3f27f
Florian Duderstadt <flori.duderstadt@gmail.com>
2023-10-12 17:51:06 +0200

    delete all not needed links in the navigationbar

--- webclient/app/src/app/AppConfig.js
+++ webclient/app/src/app/AppConfig.js
@@ -1,9 +1,5 @@
 const appItems = [
     {title: "annotator.title", link: "/"},
-    {title: "point.title", link: "/point/"},
-    {title: "polygon.title", link: "/polygon/"},
-    {title: "image.title", link: "/image/"},
-    {title: "classification.title", link: "/classification/"},
     {title: "parkingConfig.title", link: "/parkingconfig/"},
     {title: "parkingArea.title", link: "/parkingarea/"}
 ];
--- webclient/app/src/app/MainContentRouter.jsx
+++ webclient/app/src/app/MainContentRouter.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import {Route, Switch, useLocation} from "react-router-dom";
-import PointMain from "./features/point/PointMain";
-import PolygonMain from "./features/polygon/PolygonMain";
-import ImageMain from "./features/image/ImageMain";
-import ClassificationMain from "./features/classification/ClassificationMain";
 import ParkingConfigMain from "./features/parkingConfig/ParkingConfigMain";
 import ParkingAreaMain from "./features/parkingArea/ParkingAreaMain";
 import {appItems} from "./AppConfig";
@@ -26,10 +22,6 @@ function MainContentRouter() {
                     removeContentSpacer={location.pathname === "/"}
                 >
                     <Route exact path={"/"} component={ImageAnnotate}/>
-                    <Route path={"/point"} component={PointMain}/>
-                    <Route path={"/polygon"} component={PolygonMain}/>
-                    <Route path={"/image"} component={ImageMain}/>
-                    <Route path={"/classification"} component={ClassificationMain}/>
                     <Route path={"/parkingconfig"} component={ParkingConfigMain}/>
                     <Route path={"/parkingarea"} component={ParkingAreaMain}/>
                 </Navigation>
