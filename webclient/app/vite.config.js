eb4b1d3a42ee689ac2a3d99f0ccc8db01f4b9417
flonix8 <flstanek@googlemail.com>
2023-10-24 17:33:53 +0200

    Fix static base path (vite could already do relative base paths...)

--- webclient/app/vite.config.js
+++ webclient/app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smartparkingconfig',
+  base: './',
   server: {
     proxy: {
       '/api': 'http://localhost:8081/smartparkingconfig'
