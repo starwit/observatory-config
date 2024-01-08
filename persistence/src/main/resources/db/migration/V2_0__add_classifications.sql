021712d3ca32df974a17aae9b2f8e0b23ebcb90b
flonix8 <flstanek@googlemail.com>
2024-01-08 18:27:33 +0100

    Update react-image-annotate to 4.2.1

--- persistence/src/main/resources/db/migration/V2_0__add_classifications.sql
+++ persistence/src/main/resources/db/migration/V2_0__add_classifications.sql
@@ -1,9 +1,3 @@
 INSERT INTO "classification"(
 	"name")
-	VALUES ('Überwachungsbereich');
-INSERT INTO "classification"(
-	"name")
-	VALUES ('Sperrfläche');
-INSERT INTO "classification"(
-	"name")
-	VALUES ('Parkbereich');
+	VALUES ('Lichtschranke');
