c7832a67635e780fb768521d243782a26be7a39c
witchpou <anett_huebner@gmx.de>
2024-01-24 15:45:37 +0100

    ab#1022: drawing tool per classifications

--- service/src/main/java/de/starwit/service/impl/DatabackendService.java
+++ service/src/main/java/de/starwit/service/impl/DatabackendService.java
@@ -24,7 +24,7 @@ import de.starwit.service.dto.RegionDto;
 public class DatabackendService {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
-    
+
     private URI databackendUri;
 
     private RestClient restClient;
@@ -34,7 +34,8 @@ public class DatabackendService {
 
     public DatabackendService(@Value("${databackend.url}") URI configuredUri) {
         restClient = RestClient.create();
-        // This is a workaround to make sure the URI ends in a "/", s.t. resolve() works properly further down
+        // This is a workaround to make sure the URI ends in a "/", s.t. resolve() works
+        // properly further down
         this.databackendUri = URI.create(configuredUri.toString() + "/").resolve("");
     }
 
@@ -42,9 +43,9 @@ public class DatabackendService {
     public void syncConfiguration(ImageDto imageDto) {
         try {
             ResponseEntity<Void> deleteResponse = restClient.delete()
-                .uri(databackendUri.resolve("api/analytics-job/all"))
-                .retrieve().toBodilessEntity();
-            
+                    .uri(databackendUri.resolve("api/analytics-job/all"))
+                    .retrieve().toBodilessEntity();
+
             log.info("Cleared databackend configuration: HTTP " + deleteResponse.getStatusCode());
         } catch (RestClientResponseException | ResourceAccessException ex) {
             log.error("Could not clear existing jobs on databackend", ex);
@@ -52,14 +53,14 @@ public class DatabackendService {
         }
 
         for (RegionDto regionDto : imageDto.getRegions()) {
-            try {                
+            try {
                 DatabackendDto dto = toDatabackendDto(imageDto, regionDto);
-                
+
                 ResponseEntity<Void> postResponse = restClient.post()
-                    .uri(databackendUri.resolve("api/analytics-job"))
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(dto)
-                    .retrieve().toBodilessEntity();
+                        .uri(databackendUri.resolve("api/analytics-job"))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(dto)
+                        .retrieve().toBodilessEntity();
 
                 log.info("Successfully sent configuration to databackend: HTTP " + postResponse.getStatusCode());
             } catch (IllegalGeometryException e) {
@@ -78,6 +79,7 @@ public class DatabackendService {
         dbeDto.setDetectionClassId(2);
         dbeDto.setEnabled(true);
         dbeDto.setParkingAreaId(1L);
+        dbeDto.setCls(cameraId);
 
         List<GeometryPointsDto> geometryPoints = new ArrayList<>();
 
@@ -100,7 +102,7 @@ public class DatabackendService {
             dbeDto.setType("AREA_OCCUPANCY");
             for (int i = 0; i < regionDto.getPoints().size(); i++) {
                 GeometryPointsDto point = new GeometryPointsDto();
-                point.setX(regionDto.getPoints().get(i).get(0) * imageDto.getImageWidth());            
+                point.setX(regionDto.getPoints().get(i).get(0) * imageDto.getImageWidth());
                 point.setY(regionDto.getPoints().get(i).get(1) * imageDto.getImageHeight());
                 point.setOrderIdx(i);
                 geometryPoints.add(point);
@@ -111,9 +113,10 @@ public class DatabackendService {
         }
 
         dbeDto.setGeometryPoints(geometryPoints);
-        
+
         return dbeDto;
     }
 
-    class IllegalGeometryException extends Exception {};
+    class IllegalGeometryException extends Exception {
+    };
 }
