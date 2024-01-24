149a93a63bd6a555908d75fe5b09a911e3b23c86
witchpou <anett_huebner@gmx.de>
2024-01-24 12:08:39 +0100

    ab#1022: added classifications

---
+++ persistence/src/main/resources/db/migration/V10_0__changeclassifications.sql
@@ -0,0 +1,11 @@
+DELETE FROM "classification";
+
+INSERT INTO "classification"(
+	"name", "color")
+	VALUES ('Lichtschranke', '#f44336');
+INSERT INTO "classification"(
+	"name", "color")
+	VALUES ('Sperrfläche', '#e9881e');
+INSERT INTO "classification"(
+	"name, "color")
+	VALUES ('Parkbereich', '#1e87e9');
