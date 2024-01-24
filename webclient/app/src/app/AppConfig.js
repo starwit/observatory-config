65ceec630f4c4308310ef1b8599bbda46036b777
witchpou <anett_huebner@gmx.de>
2024-01-24 17:02:21 +0100

    ab#1022: drawing tool per classifications

--- webclient/app/src/app/AppConfig.js
+++ webclient/app/src/app/AppConfig.js
@@ -1,13 +1,7 @@
-const appItems = [
-    {title: "annotator.title", link: "/"},
-    {title: "parkingConfig.title", link: "/parkingconfig/"},
-    {title: "parkingArea.title", link: "/parkingarea/"}
-];
-
 const classificationSelectTools = [
     {classification: "Lichtschranke", selectTool: "create-line"},
     {classification: "Sperrfläche", selectTool: "create-polygon"},
     {classification: "Parkbereich", selectTool: "create-polygon"}
 ];
 
-export {appItems, classificationSelectTools};
+export {classificationSelectTools};
