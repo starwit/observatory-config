30507ee666c71ba75f4240270203f7e81b84d938
flonix8 <flstanek@googlemail.com>
2024-01-24 14:31:36 +0100

    Remove some whitespace and imports; Add warning about weird behavior

--- webclient/app/src/app/MainContentRouter.jsx
+++ webclient/app/src/app/MainContentRouter.jsx
@@ -1,24 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {Route, Routes} from "react-router-dom";
 import ImageAnnotate from "./features/imageAnnotate/ImageAnnotate";
-import {useTranslation} from "react-i18next";
 import Navigation from "./commons/navigation/Navigation";
-import logo from "./assets/images/logo-white.png";
-import {useLocation, useNavigate} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
 function MainContentRouter() {
 
-    const {t} = useTranslation();
-    const location = useLocation();
+    // I have absolutely no idea why, but if you remove this, `navigate()` in `ParkingAreaSelect` breaks...
+    useLocation();
 
     return (
         <>
-            <Navigation
-  
-            >
+            <Navigation>
                 <Routes>
                     <Route path={"/:imageId"} element={<ImageAnnotate />}/>
- 
                 </Routes>
             </Navigation>
         </>
