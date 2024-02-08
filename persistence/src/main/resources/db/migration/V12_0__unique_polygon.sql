e7d649693951a6ea4ac716a8ecc545185af33603
flonix8 <flstanek@googlemail.com>
2024-02-08 17:42:39 +0100

    Fix DB migration script (make it compatible with H2 for testing)

--- persistence/src/main/resources/db/migration/V12_0__unique_polygon.sql
+++ persistence/src/main/resources/db/migration/V12_0__unique_polygon.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "polygon"
-    ADD CONSTRAINT "polygon_name_image" UNIQUE ("name", "image_id"),
+    ADD CONSTRAINT "polygon_name_image" UNIQUE ("name", "image_id");
+ALTER TABLE "polygon"
     ALTER COLUMN "name" SET NOT NULL;
