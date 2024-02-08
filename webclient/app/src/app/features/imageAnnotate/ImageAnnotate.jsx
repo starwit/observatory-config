f8e2df0d033171036ba3f2df1c862127e570a4c0
witchpou <anett_huebner@gmx.de>
2024-02-08 22:59:01 +0100

    feature/ab#995: making parking area selegt a bit nicer

--- webclient/app/src/app/features/imageAnnotate/ImageAnnotate.jsx
+++ webclient/app/src/app/features/imageAnnotate/ImageAnnotate.jsx
@@ -104,7 +104,7 @@ function ImageAnnotate() {
     if (!classifications || !images) {
         return (
             <>
-                <AppBar sx={{bgcolor: "white", color: "black", width: "100%"}}>
+                <AppBar sx={{bgcolor: "white", color: "black", width: "100%", left: "0rem", paddingLeft: "3rem"}}>
                     <ParkingAreaSelect />
                 </AppBar>
                 <Box sx={{padding: "3rem", paddingTop: "5rem"}}>
@@ -117,8 +117,8 @@ function ImageAnnotate() {
     if (images.length === 0) {
         return (
             <>
-                <AppBar sx={{bgcolor: "white", color: "black", width: "100%"}}>
-                    <ParkingAreaSelect />
+                <AppBar sx={{bgcolor: "white", color: "black", width: "100%", left: "0rem", paddingLeft: "3rem"}}>
+                    <ParkingAreaSelect sx={{}} />
                 </AppBar>
                 <Box sx={{padding: "3rem", paddingTop: "5rem"}}>
                     <Typography variant="h5">{t("parkingConfig.image.empty")}</Typography>
@@ -130,7 +130,7 @@ function ImageAnnotate() {
 
     return (
         <>
-            <AppBar color="transparent" sx={{boxShadow: "none", width: "100%", right: "8rem", left: "0rem"}}>
+            <AppBar color="transparent" sx={{boxShadow: "none", left: "3rem", right: "8rem", width: "80%"}}>
                 <ParkingAreaSelect />
             </AppBar>
             <ReactImageAnnotate
