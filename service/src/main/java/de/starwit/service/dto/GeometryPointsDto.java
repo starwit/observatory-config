5f4aae4d930ad4ac28865c3ff95dd04c15d2c49f
Florian Duderstadt <flori.duderstadt@gmail.com>
2024-02-28 16:29:13 +0100

    adjust DTOs to geo coordinates

--- service/src/main/java/de/starwit/service/dto/GeometryPointsDto.java
+++ service/src/main/java/de/starwit/service/dto/GeometryPointsDto.java
@@ -1,8 +1,12 @@
 package de.starwit.service.dto;
 
+import java.math.BigDecimal;
+
 public class GeometryPointsDto {
     private Double x;
     private Double y;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
     private Integer orderIdx;
 
     public Double getX() {
@@ -20,7 +24,22 @@ public class GeometryPointsDto {
     public void setY(Double y) {
         this.y = y;
     }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
     
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
     public Integer getOrderIdx() {
         return orderIdx;
     }
--- service/src/main/java/de/starwit/service/dto/ImageDto.java
+++ service/src/main/java/de/starwit/service/dto/ImageDto.java
@@ -1,5 +1,6 @@
 package de.starwit.service.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import de.starwit.persistence.entity.AbstractEntity;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ImageDto extends AbstractEntity<Long> {
@@ -22,6 +26,56 @@ public class ImageDto extends AbstractEntity<Long> {
     private int imageHeight;
     
     private int imageWidth;
+    
+    private BigDecimal degreeperpixelx;
+
+    private BigDecimal degreeperpixely;
+
+    private Boolean geoReferenced;
+
+    private BigDecimal topleftlongitude;
+
+    private BigDecimal topleftlatitude;
+
+    public BigDecimal getTopleftlatitude() {
+        return topleftlatitude;
+    }
+
+    public void setTopleftlatitude(BigDecimal topleftlatitude) {
+        this.topleftlatitude = topleftlatitude;
+    }
+
+    public BigDecimal getTopleftlongitude() {
+        return topleftlongitude;
+    }
+
+    public void setTopleftlongitude(BigDecimal topleftlongitude) {
+        this.topleftlongitude = topleftlongitude;
+    }
+
+    public Boolean getGeoReferenced() {
+        return geoReferenced;
+    }
+
+    public void setGeoReferenced(Boolean geoReferenced) {
+        this.geoReferenced = geoReferenced;
+    }
+
+    public BigDecimal getDegreeperpixelx() {
+        return degreeperpixelx;
+    }
+
+    public void setDegreeperpixelx(BigDecimal degreeperpixelx) {
+        this.degreeperpixelx = degreeperpixelx;
+    }
+
+    public BigDecimal getDegreeperpixely() {
+        return degreeperpixely;
+    }
+
+    public void setDegreeperpixely(BigDecimal degreeperpixely) {
+        this.degreeperpixely = degreeperpixely;
+    }
 
     public byte[] getData() {
         return data;
