9c2207cb5f9f591a3d265fad247d67d9551076d4
Anett <anett.huebner@starwit.de>
2024-07-03 10:15:57 +0200

    removed old docker scripts for deployment

--- deployment/https/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DB_PW_smartparkingconfig=
-export KEYCLOAK_PW=
-export DB_PW_KEYCLOAK=
-export DB_PW_ROOT=
-export DB_PW_KC_ROOT=
-export KEYCLOAK_USER="smartparkingconfigadmin"
-export DOMAIN="your.domain.de"
-export EMAIL="test@test.de"
